77a552fab4109f23d556a957455079ef5fe46942
AsthaKumari07 <asthas274@gmail.com>
2025-06-05 11:07:38 +0530

    new changes

--- Client/Server/server.js
+++ Client/Server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const path = require("path");
 
 dotenv.config();
 const app = express();
@@ -12,6 +13,14 @@ connectDB();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+const _dirname = path.dirname("");
+const buildpath = path.join(_dirname, "../Client/dist");
+app.use(express.static(buildpath));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // Routes
 app.use("/api/medicines", require("./routes/medicineRoutes"));
